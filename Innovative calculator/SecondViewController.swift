75dee7b8875803ebc963ee2696dd32db783071d6
Jordan Byrne <jordan5039@live.com>
2014-11-25 14:21:36 +0000

    Initial Commit

---
+++ Innovative calculator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Innovative calculator
+//
+//  Created by jordan on 25/11/2014.
+//  Copyright (c) 2014 jordan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
